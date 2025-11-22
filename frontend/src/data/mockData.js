30985520af2b315846ff7d012b478eccc22572a3
Casen-DG <dingxigu111@gmail.com>
2025-11-21 21:39:21 -0700

    Day 2: Frontend UI completed

---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,12 @@
+// frontend/src/data/mockData.js
+export const MOCK_CARD = {
+  id: "101",
+  title: "🟢 什么是 React Hooks？",
+  content: [
+    "1. 核心：让函数组件也能使用 State 和生命周期。",
+    "2. 常用：useState (状态), useEffect (副作用)。",
+    "3. 规则：只能在顶层调用，不能在循环/判断中。",
+    "4. 优势：代码复用更方便，逻辑更清晰。"
+  ],
+  tags: ["React", "核心原理", "高频"]
+};
